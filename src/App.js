9b2f8f8945c0fac23803fc8df1b02d7984268da1
Stanislau Marudau <marudov.stas@gmail.com>
2023-07-23 20:44:55 +0300

    Functionality fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
-function App() {
+const App = () => {
+  const [step, setStep] = useState(1);
+
+  const handlePrevious = () => {
+    if (step >= 1) {
+      setStep(step - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (step <= 3) {
+      setStep(step + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step === 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          onClick={handlePrevious}
+          style={{ backgroundColor: "#7950f2", color: "white" }}
         >
-          Learn React
-        </a>
-      </header>
+          Previous
+        </button>
+        <button
+          onClick={handleNext}
+          style={{ backgroundColor: "#7950f2", color: "white" }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
